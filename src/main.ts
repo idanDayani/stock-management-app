929f599086e2eda5eb6e8248b7a116ae0d70d3d5
IdanDayani8 <idand@yuvital.com>
2024-08-10 11:56:09 +0300

    Add cors config

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const frontEndUrl = 'http://localhost:3001';
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: frontEndUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
